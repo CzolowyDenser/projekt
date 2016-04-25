61dacf73c6565ea22ba029eb9e7664084aac13e7
CzolowyDenser <mrokuuuu@gmil.com>
2016-04-25 15:00:27 +0200

    do nagrody

--- projekt/projekt.cpp
+++ projekt/projekt.cpp
@@ -7,7 +7,7 @@
 #include <time.h>
 #include <cstdlib>
 #include <fstream>
-
+#include <windows.h>
 
 using namespace std;
 
@@ -17,69 +17,67 @@ int main()
 {
 
 	string haslo;
+	string hasloKropki;
 	string random;
 	string kategoria;
 	string graczOne;
 	string graczTwo;
+	string nagroda;
 	int nrHasla = 0;
 	int dlHasla = 0;
-	int i = 0;
-	int l = 1;
+	int nrNagrody = 0;
+	
 	srand(time(NULL));
-
+//wczytanie imion graczy
 	cout << "KOLO FORTUNY v0.1\n\n";
 	cout << "Podaj imie pierwszego gracza:\n";
 	getline(cin, graczOne);
 
 	cout << "Podaj imie drugiego gracza:\n";
 	getline(cin, graczTwo);
-
+	Sleep(800);
+	system("cls");
 	cout << "\nWitamy\n" << graczOne << " rozpoczyna kolejke.\n\nHaslo z kategori: \n";
 
 
-
+	
+//wylosowanie hasla i kategori z pliku
 	ifstream plik("hasla.txt");
 	nrHasla = (rand() % 5) + 1;
 	for (int i = 1; i < nrHasla; i++)
-	{
-		getline(plik, random);
-	}
-	
-	
-	plik >> kategoria;
-	
-	plik >> haslo;
-		//getline(plik, kategoria);
-	
-
-	cout << kategoria;
-	cout << endl;
-	cout << haslo;
-
-
-	
-	/*while (isupper(kategoria.at(i)))
-	{
-		cout << kategoria.at(i);
-
-		i++;
-	}
-	if (isspace(kategoria.at(i)))
-	{
-		i++;
-		do
 		{
-			haslo.at(l) = kategoria.at(i);
-			i++;
-			l++;
-		} while (!kategoria.at(i) == '.');
+			getline(plik, random);
+		}
+//wczytanie	hasla i kategori do osobnych zmiennych
+	plik >> kategoria;
+	plik >> haslo;
+	plik.close();
+	cout << kategoria<<endl;
+//podmiana hasla na kropki
+	dlHasla = haslo.size();
+	hasloKropki.append(dlHasla, '.');
 
+//losowanie nagrody z pliku	
+	cout << "Nacisnij ENTER aby zakrecic kolem" << endl;
+	cin.get();
+	ifstream prize("nagrody.txt");
+	nrNagrody = (rand() % 12) + 1;
+	for (int i = 1; i < nrNagrody; i++)
+	{
+		prize >> nagroda;
 	}
+	prize.close();
+	cout << "Wylosowales/as " << nagroda << endl;
 
-	cout << endl << haslo;
 
+
+	
 	cout << endl;
-*/
+
+	
+
+
+	
 	cin.get();
 	cin.get();
 	return 0;
