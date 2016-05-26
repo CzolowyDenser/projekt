681acf3d0510962cc124c90c7665ed7f2413bded
CzolowyDenser <mrokuuuu@gmail.com>
2016-05-26 16:32:32 +0200

    asd

--- projekt/projekt.cpp
+++ projekt/projekt.cpp
@@ -8,14 +8,18 @@
 #include <cstdlib>
 #include <fstream>
 #include <windows.h>
+#include <cctype>
+
 using namespace std;
 
 const int n = 30;										//ilosc nagrod
 const int p = 5;										//ilosc pytan
-const int r = 4;										//ilosc rund
+const int r = 3;										//ilosc rund
+int k = 0;				
+string sprawdzHaslo;									
 
-void kolejkaGraczaPierwszego(string, string&, int);
-void kolejkaGraczaDrugiego(string, string&, int);
+void kolejkaGraczaPierwszego(string, string&, int, string);
+void kolejkaGraczaDrugiego(string, string&, int, string);
 int nagrodaa();
 void wyswietlRanking();
 
@@ -48,9 +52,17 @@ int main()
 		cout << "KOLO FORTUNY v0.5\n\n\n";
 		cout << "1. Rozpocznij gre" << endl;
 		cout << "2. Sprawdz tabele wynikow" << endl;
-		cout << "3. Wyjscie" << endl;
-		int menu = 0;
-		cin >> menu;
+		cout << "3. Zasady gry." << endl;
+		cout << "4. Wyjscie" << endl;
+		int menu = 0;	
+		while (!(cin>>menu))
+			{
+				cin.clear();
+				cin.ignore();
+				cout << "Zla wartosc" << endl;
+				cout << "Wybierz jeszcze raz: ";
+			}
+		
 		switch (menu)
 		{
 		case 1:
@@ -63,7 +75,7 @@ int main()
 			getline(cin, dwa.imie);
 			Sleep(800);
 			system("cls");
-			cout << "\nWitamy\n" << jeden.imie << " rozpoczyna kolejke.\n\nHaslo z kategori: \n";
+			cout << "\nWitamy\n" << jeden.imie << " rozpoczyna kolejke.\n\n\n\t\t\tHaslo z kategori: \n\n";
 			int b = 1;
 			for (b = 1; b <= r; b++)
 			{
@@ -88,32 +100,60 @@ int main()
 				kategoria.clear();
 				haslo.clear();
 				plik >> kategoria;																					//wczytanie	hasla i kategori do osobnych zmiennych
-				plik >> haslo;
+				getline(plik, haslo);
+				haslo.erase(0, 1);
 				plik.close();
-				cout << kategoria << endl;
+				cout << "\t\t\t\t" << kategoria << "\n\n\n";
 				hasloKropki.clear();
 				hasloKropki = haslo;
 				dlHasla = haslo.size();																				
 				for (int i = 0; i <= dlHasla; i++)																	//podmiana hasla na kropki
 				{
-					if (hasloKropki[i] == '-');
+					
+					if (hasloKropki[i] == ' ')
+						hasloKropki[i] = '-';
 					else
 						hasloKropki[i] = '.';
+				}
+				for (int i = 0; i <= dlHasla; i++)																
+				{
 
+					if (haslo[i] == ' ')
+						haslo[i] = '-';
 				}
 
 				do
 				{
-					kolejkaGraczaPierwszego(haslo, hasloKropki, dlHasla);
+					if (k == 0)
+					{
+						kolejkaGraczaPierwszego(haslo, hasloKropki, dlHasla, kategoria);
 
-					if (haslo == hasloKropki)
-						break;
-
-					kolejkaGraczaDrugiego(haslo, hasloKropki, dlHasla);
+						if (haslo == hasloKropki || haslo == sprawdzHaslo)
+							break;
+					}
+					if (k == 1) kolejkaGraczaDrugiego(haslo, hasloKropki, dlHasla, kategoria);
 
 				} while (haslo != hasloKropki);
 
 			}
+			system("cls");
+			if (jeden.pieniadze > dwa.pieniadze)
+			{
+				cout << "Koniec gry!" << endl;
+				cout << "Wygrywa " << jeden.imie<< " z wynikiem: " << jeden.pieniadze << "\nGratulacje!";
+			}
+			else
+				if (jeden.pieniadze < dwa.pieniadze)
+				{
+					cout << "Koniec gry!" << endl;
+					cout << "Wygrywa " << dwa.imie<< " z wynikiem: "<< dwa.pieniadze << "\nGratulacje!";
+				}
+				else
+					if (jeden.pieniadze == dwa.pieniadze)
+						cout << "REMIS!" << endl;
+			cout << "\nZa chwile zobaczysz ranking graczy.";
+			Sleep(6000);
+			
 
 			ofstream ranko("ranking.txt", ios::app);
 			ranko << jeden.pieniadze << " " << jeden.imie << endl;
@@ -129,6 +169,20 @@ int main()
 			break;
 		}
 		case 3:
+		{
+			system("cls");
+			cout << "1. Gra przeznaczona dla dwoch graczy." << endl << endl;;
+			cout << "2. Spolgloski mozna podawac, a samogloski sie kupuje." << endl << endl;
+			cout << "3. Koszt samogloski 50." << endl << endl;
+			cout << "4. Jesli podasz samogloske, a nie masz wystarczajacej ilosci pieniedzy na koncie " << endl;
+			cout << "   stracisz kolejke." << endl << endl;
+			cout << "5. Przy podawaniu calego hasla nagroda to 50 za kazda litere hasla." << endl << endl;
+			cout << "6. Aby podac haslo w calosci nalezy zamiast litery wpisac 0 (zero)." << endl << endl;
+			cin.get();
+			cin.get();
+			break;
+		}
+		case 4:
 			exit(0);
 		}
 	} while (true);
@@ -152,11 +206,12 @@ int nagrodaa()
 		return nagroda;
 	}
 
-void kolejkaGraczaPierwszego(string hasloo, string& hasloKropkii, int dlHaslaa)
+void kolejkaGraczaPierwszego(string hasloo, string& hasloKropkii, int dlHaslaa, string kat)
 	{
 		char litera;
 		int pula;
 		bool w = 0;
+		
 		do
 		{
 			cout << jeden.imie << " ";
@@ -168,10 +223,51 @@ void kolejkaGraczaPierwszego(string hasloo, string& hasloKropkii, int dlHaslaa)
 				}
 			w = 0;
 			cout << endl;
-			cout << jeden.imie << " podaj litere." << endl;
+			cout << endl;
+			cout  << "\t\t\t" << kat<<"  " << hasloKropkii << "\n\n";
+			cout << jeden.imie << " podaj litere. Aby wprowadzic cale haslo podaj 0." << endl;
 			cin >> litera;
 			cout << endl;
+			cin.sync();
+			cin.clear();
 			cin.get();
+			if (litera == 'a' || litera == 'e' || litera == 'y' || litera == 'u' || litera == 'i' || litera == 'o')
+			{
+				if (jeden.pieniadze < 50)
+				{
+					cout << "Nie mozesz podac samogloski. Brak srodkow na koncie. Tracisz kolejke." << endl;
+					k = 1;
+					break;
+				}
+				cout << "Podales samogloske. Koszt 50" << endl;
+				jeden.pieniadze -= 50;
+			}
+			if (litera == '0')
+			{
+				cout << "Podaj cale haslo:" << endl;
+				getline(cin, sprawdzHaslo);
+				if (sprawdzHaslo == hasloo)
+				{
+					int c = 0;
+					cout << "HASLO PRAWIDLOWE" << endl;
+					hasloKropkii = hasloo;
+					for (int i = 0; i <= dlHaslaa; i++)
+					{
+						if (isalpha(hasloo[i]))
+							c++;
+					}
+					jeden.pieniadze += (c * 50);
+					cout << "Twoj stan konta " << jeden.pieniadze << endl;
+					break;
+				}
+				else
+				{
+					cout << "HASLO NIE PRAWIDLOWE" << endl;
+					k = 1;
+					break;
+				}
+			}
+
 
 			for (int i = 0; i <= dlHaslaa; i++)
 				{
@@ -180,23 +276,31 @@ void kolejkaGraczaPierwszego(string hasloo, string& hasloKropkii, int dlHaslaa)
 						hasloKropkii[i] = litera;
 						w = 1;
 						jeden.pieniadze += pula;
-						cout << "BRAWO! Twoj stan konta: " << jeden.pieniadze << endl;
-			
+						cout << "BRAWO! Twoj stan konta: " << jeden.pieniadze << "\n\n";
 					}	
 				}
-			cout << hasloKropkii << endl;
-			if (hasloKropkii == hasloo)
-				w=0;
+			cout << "\t\t\t" << kat<<"  " << hasloKropkii << "\n\n";
+			if (w == 0)
+			{
+				cout << "Brak podanej litery :(" << endl;
+				k = 1;
+				Sleep(2000);
+				system("cls");
+				break;
+			}
+			if (hasloKropkii == hasloo || sprawdzHaslo == hasloo)
+				w = 0;
 
 		} while (w==1);
-
+		
 	}
 
-void kolejkaGraczaDrugiego(string hasloo, string& hasloKropkii, int dlHaslaa)
+void kolejkaGraczaDrugiego(string hasloo, string& hasloKropkii, int dlHaslaa, string kat)
 {
 	char litera;
 	int pula;
 	bool w = 0;
+
 	do
 	{
 		cout << dwa.imie << " ";
@@ -208,11 +312,51 @@ void kolejkaGraczaDrugiego(string hasloo, string& hasloKropkii, int dlHaslaa)
 		}
 		w = 0;
 		cout << endl;
-		cout << dwa.imie << " podaj litere." << endl;
+		cout << endl;
+		cout <<  "\t\t\t" << kat<<"  " << hasloKropkii << "\n\n";
+		cout << dwa.imie << " podaj litere. Aby wprowadzic cale haslo podaj 0." << endl;
 		cin >> litera;
 		cout << endl;
+		cin.sync();
+		cin.clear();
 		cin.get();
+		if (litera == 'a' || litera == 'e' || litera == 'y' || litera == 'u' || litera == 'i' || litera == 'o')
+		{
+			if (dwa.pieniadze < 50)
+			{
+				cout << "Nie mozesz podac samogloski. Brak srodkow na koncie. Tracisz kolejke." << endl;
+				k = 0;
+				break;
+			}
+			cout << "Podales samogloske. Koszt 50" << endl;
+			dwa.pieniadze -= 50;
+		}
 
+		if (litera == '0')
+		{
+			cout << "Podaj cale haslo:" << endl;
+			getline(cin, sprawdzHaslo);
+			if (sprawdzHaslo == hasloo)
+			{
+				int c = 0;
+				cout << "HASLO PRAWIDLOWE" << endl;
+				hasloKropkii = hasloo;
+				for (int i = 0; i <= dlHaslaa; i++)
+				{
+					if (isalpha(hasloo[i]))
+						c++;
+				}
+				dwa.pieniadze += (c * 50);
+				cout << "Twoj stan konta " << dwa.pieniadze << endl;
+				break;
+			}
+			else
+			{
+				cout << "HASLO NIE PRAWIDLOWE" << endl;
+				k = 0;
+				break;
+			}
+		}
 		for (int i = 0; i <= dlHaslaa; i++)
 		{
 			if (hasloo[i] == litera)
@@ -220,12 +364,21 @@ void kolejkaGraczaDrugiego(string hasloo, string& hasloKropkii, int dlHaslaa)
 				hasloKropkii[i] = litera;
 				w = 1;
 				dwa.pieniadze += pula;
-				cout << "BRAWO! Twoj stan konta: " << dwa.pieniadze << endl;
+				cout << "BRAWO! Twoj stan konta: " << dwa.pieniadze << "\n\n";
 			}
 		}
-		cout << hasloKropkii << endl;
-		if (hasloKropkii == hasloo)
+		cout << "\t\t\t" << kat<< "  "<< hasloKropkii << "\n\n";
+		if (w == 0)
+		{
+			cout << "Brak podanej litery :(" << endl;
+			k = 0;
+			Sleep(2000);
+			system("cls");
+			break;
+		}
+		if (hasloKropkii == hasloo || sprawdzHaslo == hasloo)
 			w=0;
+		
 	} while (w == 1);
 
 }
@@ -238,7 +391,7 @@ void wyswietlRanking()
 		int pieniadze;
 	};
 	string test;
-	int b = 0;
+	int b = -1;
 
 	system("cls");
 
@@ -248,7 +401,6 @@ void wyswietlRanking()
 		getline(rank, test);
 		b++;
 	} while (!rank.eof());
-	b--;
 	rank.close();
 
 	gracz* tab = new gracz[b];
